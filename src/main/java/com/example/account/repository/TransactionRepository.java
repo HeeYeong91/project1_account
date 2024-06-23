473f8c574444e4bf0c94d0f161545c220a4e3dbb
Heeyeong <gmldud5659@naver.com>
2024-06-24 04:52:44 +0900

    :sparkles: 잔액 사용 취소 API

--- src/main/java/com/example/account/repository/TransactionRepository.java
+++ src/main/java/com/example/account/repository/TransactionRepository.java
@@ -4,6 +4,8 @@ import com.example.account.domain.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 /**
  * Transaction Repository
  * @author 이희영
@@ -11,5 +13,10 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
-    
+    /**
+     * 트랜잭션 아이디로 트랜잭션 찾기
+     * @param transactionId 트랜잭션 아이디
+     * @return 트랜잭션
+     */
+    Optional<Transaction> findByTransactionId(String transactionId);
 }
